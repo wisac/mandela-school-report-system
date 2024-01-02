fef5d14f0cf35eecb0275d3a2b3f9175424276d3
Wemegah Sherif <omegajunior63@gmail.com>
2024-01-02 12:02:24 +0000

    CCC

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -55,11 +55,7 @@
 						<div class="col-xl-12 tab-content">
 							<div id="sign-up" class="auth-form tab-pane fade show active  form-validation">
 								@if(session()->has('error'))
-									<div class="alert alert-danger alert-dismissible fade show">
-										<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="me-2"><polygon points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2"></polygon><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>
-										<strong>Error!</strong> {{session('error')}}
-										<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="btn-close"><span><i class="fa-solid fa-xmark"></i></span>
-										</button>
+									 
 									</div>
 									@endif 
 							
